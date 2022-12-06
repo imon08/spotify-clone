bbf150ac9f0a49edc43c9bd77c4f11b088c3cbf3
Priyosmita <priyosh08@gmail.com>
2022-12-06 17:30:21 +0530

    login page

---
+++ src/spotify.js
@@ -0,0 +1,15 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "5f29ab207cb745f79daf2e8d7effe9c1";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+  ];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
