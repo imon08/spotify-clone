f77ff663106d14be7fe1d4666e1b7707df96ddd4
Priyosmita <priyosh08@gmail.com>
2022-12-07 15:31:27 +0530

    logged in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  //Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log("Person: ", user);
+      });
+    }
+    //console.log("I have a toke: ", token);
+  }, []);
+
   return (
-    <div className="app">
-      {/* Spotify logo */}
-      {/* login with spotify button */}
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 }
 
--- src/spotify.js
+++ src/spotify.js
@@ -3,12 +3,25 @@ const redirectUri = "http://localhost:3000/";
 const clientId = "5f29ab207cb745f79daf2e8d7effe9c1";
 
 const scopes = [
-    "user-read-currently-playing",
-    "user-read-recently-played",
-    "user-read-playback-state",
-    "user-top-read",
-    "user-modify-playback-state",
-  ];
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      // #accessToken=mysupersecetkey&name=
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
